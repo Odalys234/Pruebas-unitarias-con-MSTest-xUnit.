4d2126b879f8073419fb12ae560fec1881474f5c
Odalys234 <ae24002@esfe.agape.edu.sv>
2024-10-10 14:16:06 -0600

    Pruebas unitarias con MSTest creacion del proyecto dentro de la solucion

--- KOAE.Temperatura.UnitTestMSTest/obj/Debug/net6.0/KOAE.Temperatura.UnitTestMSTest.AssemblyInfo.cs
+++ KOAE.Temperatura.UnitTestMSTest/obj/Debug/net6.0/KOAE.Temperatura.UnitTestMSTest.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("KOAE.Temperatura")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("KOAE.Temperatura.UnitTestMSTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b195a53440ed9c1948c6692b92a8c6941187c9e5")]
-[assembly: System.Reflection.AssemblyProductAttribute("KOAE.Temperatura")]
-[assembly: System.Reflection.AssemblyTitleAttribute("KOAE.Temperatura")]
+[assembly: System.Reflection.AssemblyProductAttribute("KOAE.Temperatura.UnitTestMSTest")]
+[assembly: System.Reflection.AssemblyTitleAttribute("KOAE.Temperatura.UnitTestMSTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
